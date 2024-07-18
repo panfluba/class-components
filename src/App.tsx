1533f7a8c7acb1a569dcf033470f4abd5dffc187
lusia <panfluba@gmail.com>
2024-07-18 12:29:32 +0300

    fix: fix loading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 // App.tsx
 import React from 'react';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
 import NotFound from './components/NotFound/NotFound';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SearchPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 };
 
