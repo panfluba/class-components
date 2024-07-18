02b57ea5f1f4c5bb07d981e268bc10e3745991cf
lusia <panfluba@gmail.com>
2024-07-18 11:56:59 +0300

    feat: update Search to FC

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,47 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './search.module.scss';
 import findIcon from '../../assets/img/search.svg';
 
-import _ from 'lodash';
-
-interface Props {
+type TSearchProps = {
   searchTerm: string;
   onSearch: (searchTerm: string) => void;
-}
+};
 
-interface State {
-  searchTerm: string;
-}
+const Search: React.FC<TSearchProps> = ({ searchTerm, onSearch }) => {
+  const [term, setTerm] = useState(searchTerm);
 
-class Search extends Component<Props, State> {
-  state: State = {
-    searchTerm: this.props.searchTerm,
-  };
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onSearch(term);
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [term, onSearch]);
 
-  debouncedSearch = _.debounce(() => {
-    this.props.onSearch(this.state.searchTerm);
-  }, 500);
-
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ searchTerm: event.target.value }, this.debouncedSearch);
-  };
-
-  render() {
-    return (
-      <div className={styles.root}>
-        <img
-          className={`${styles.icon} ${styles.icon__find}`}
-          src={findIcon}
-        ></img>
-        <input
-          className={styles.input}
-          type="text"
-          value={this.state.searchTerm}
-          onChange={this.handleChange}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.root}>
+      <img
+        className={`${styles.icon} ${styles.icon__find}`}
+        src={findIcon}
+        alt="search-icon"
+      />
+      <input
+        className={styles.input}
+        type="text"
+        value={term}
+        onChange={e => setTerm(e.target.value)}
+      />
+    </div>
+  );
+};
 
 export default Search;
